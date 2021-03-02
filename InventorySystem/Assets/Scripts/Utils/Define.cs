2a6cd78dab1688546fdb75faf912e92180b1e9bd
conmoto <conmoto33@naver.com>
2021-03-02 20:25:06 +0900

    Setup / Player / Camera

---
+++ InventorySystem/Assets/Scripts/Utils/Define.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+
+    public enum UIEvent
+    {
+        Click,
+        Drag,
+    }
+
+    public enum MouseEvent
+    {
+        Press,
+        Click,
+    }
+
+}
